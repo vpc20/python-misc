523429d230b955de0d635ab6c53b258f42488358
vpc <cuevasvp@gmail.com>
2020-06-25 06:55:35 +0800

    refactor

--- FlattenGenerator.py
+++ FlattenGenerator.py
@@ -1,16 +1,23 @@
+# def flatten(lst):
+#     for item in lst:
+#         if isinstance(item, list):
+#             for i in flatten(item):
+#                 yield i
+#         else:
+#             yield item
+
+
 def flatten(lst):
-    for item in lst:
-        if isinstance(item, list):
-            for i in flatten(item):
-                yield i
+    for e in lst:
+        if isinstance(e, list):
+            yield from flatten(e)
         else:
-            yield item
+            yield e
 
 
 # list_of_lists = [1, 2]
 # list_of_lists = [[1, 2], 3]
-#list_of_lists = [[1, 2], [3, 4], [5, [6, 7]]]
-list_of_lists = [[1, 2], [3, 4], [5, 6, [7,8]]]
+# list_of_lists = [[1, 2], [3, 4], [5, [6, 7]]]
+list_of_lists = [[1, 2], [3, 4], [5, 6, [7, 8]]]
 
-for x in flatten(list_of_lists):
-    print(x)
+print(list(flatten(list_of_lists)))
